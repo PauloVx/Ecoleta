532ed5b5e880e57a23f70fa0a1c55e3f660bd67e
Paulo <paulituvitu@hotmail.com>
2020-06-02 18:07:04 -0300

    Rota de listagem de itens

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import knex from './database/connection'
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-	return response.json({msg: 'Hello'});
+routes.get('/items', async (request, response) => {
+	const items = await knex('items').select('*');
+
+	const serializedItems = items.map(item => {
+		return {
+			title: item.title,
+			item_url: `http://localhost:3333/uploads/${item.image}`
+		}
+	});
+
+	return response.json({serializedItems});
 });
 
 export default routes;
